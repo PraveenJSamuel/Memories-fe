97111031c026ba6a4ac6a43721af1a17378f2aee
Praveen Samuel <prsamuel1994@gmail.com>
2023-01-01 17:32:36 -0500

    Changed axios URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://memories-api-xeqh.onrender.com' });
+const API = axios.create({ baseURL: 'https://memories-api-xeqh.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
