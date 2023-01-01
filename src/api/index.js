7ac2709140bb5b8c191bfd26ccc9b6250d31a31f
Praveen Samuel <prsamuel1994@gmail.com>
2023-01-01 17:18:01 -0500

    replaced https with http in baseURL to troubleshoot posts not loading in production build

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-app-jcx4.onrender.com' });
+const API = axios.create({ baseURL: 'http://memories-api-xeqh.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
